14c79a6ec3ba615d396d26973597c8129a5233a7
thiagocifani <cifani.thiago@gmail.com>
2019-07-24 23:41:39 -0300

    Adds unique title back to event model.

--- src/models/Event.js
+++ src/models/Event.js
@@ -5,7 +5,7 @@ const ObjectId = Schema.ObjectId;
 
 const Event = new Schema({
   id: ObjectId,
-  title: { type: Schema.Types.String, required: true },
+  title: { type: Schema.Types.String, required: true, unique: true },
   local: { type: String, required: true},
   date: Date,
   participations: [{ type: Schema.Types.ObjectId, ref: 'Participation' }]
