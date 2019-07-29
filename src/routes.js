505f3044965098b95d3a4069f78ed373aa6d58b6
thiagocifani <cifani.thiago@gmail.com>
2019-07-28 23:55:15 -0300

    Adds routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes  = express.Router();
 
 const EventController = require("./controllers/EventController");
+const StateController = require("./controllers/StateController");
+const CityController = require("./controllers/CityController");
 const UserController = require("./controllers/UserController");
 const ParticipationController = require("./controllers/ParticipationController");
 
@@ -20,11 +22,23 @@ routes.get("/users/:id", UserController.show);
 routes.put("/users/:id", UserController.update);
 routes.delete("/users/:id", UserController.destroy);
 
+routes.get("/states", StateController.index);
+routes.post("/states", StateController.create);
+routes.delete("/states/:id", StateController.destroy);
+
 const participations = express.Router({mergeParams: true});
 
 participations.post("/participations", ParticipationController.create);
 participations.delete("/participations/:id", ParticipationController.destroy);
 
 routes.use('/:events/:eventId', participations);
+
+const cities = express.Router({mergeParams: true});
+
+cities.get("/cities", CityController.index);
+cities.post("/cities", CityController.create);
+cities.delete("/cities/:id", CityController.destroy);
+
+routes.use('/:states/:stateId', cities);
 
 module.exports = routes;
